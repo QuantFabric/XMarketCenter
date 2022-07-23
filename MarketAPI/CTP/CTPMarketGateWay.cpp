03558ceb9cda3a962c52eeb71ad49732dd505f46
天山老妖 <642960662@qq.com>
2022-07-23 15:29:49 +0800

    add CTP

---
+++ MarketAPI/CTP/CTPMarketGateWay.cpp
@@ -0,0 +1,290 @@
+#include "CTPMarketGateWay.h"
+#include "XPluginEngine.hpp"
+
+CreateObjectFunc(CTPMarketGateWay);
+
+CTPMarketGateWay::CTPMarketGateWay() : MarketGateWay()
+{
+
+}
+
+bool CTPMarketGateWay::LoadAPIConfig()
+{
+    bool ret = true;
+    std::string errorBuffer;
+    if(Utils::LoadCTPMarkeSourceConfig(m_MarketCenterConfig.APIConfig.c_str(), m_CTPMarketSourceConfig, errorBuffer))
+    {
+        m_Logger->Log->info("CTPMarketGateWay::LoadCTPMarkeSourceConfig {} successed", m_MarketCenterConfig.APIConfig);
+        m_Logger->Log->info("CTPMarketGateWay FrontAddr:{} BrokerID:{} Account:{} TickerListPath:{}", 
+                            m_CTPMarketSourceConfig.FrontAddr, m_CTPMarketSourceConfig.BrokerID,
+                            m_CTPMarketSourceConfig.Account, m_CTPMarketSourceConfig.TickerListPath);
+    }
+    else
+    {
+        ret = false;
+        m_Logger->Log->error("CTPMarketGateWay::LoadCTPMarkeSourceConfig {} failed, {}", m_MarketCenterConfig.APIConfig, errorBuffer);
+    }
+    return ret;
+}
+
+void CTPMarketGateWay::Run()
+{
+    // 创建行情API实例
+    m_pMdUserApi = CThostFtdcMdApi::CreateFtdcMdApi();
+    // 注册事件类
+    m_pMdUserApi->RegisterSpi(this);
+    // 设置行情前置地址
+    m_pMdUserApi->RegisterFront(const_cast<char *>(m_CTPMarketSourceConfig.FrontAddr.c_str()));
+    m_pMdUserApi->Init();
+    m_Logger->Log->info("CTPMarketGateWay::Start FrontAddress:{}", m_CTPMarketSourceConfig.FrontAddr);
+    // 等到线程退出
+    m_pMdUserApi->Join();
+}
+
+void CTPMarketGateWay::GetCommitID(std::string& CommitID, std::string& UtilsCommitID)
+{
+    CommitID = SO_COMMITID;
+    UtilsCommitID = SO_UTILS_COMMITID;
+}
+
+void CTPMarketGateWay::GetAPIVersion(std::string& APIVersion)
+{
+    APIVersion = API_VERSION;
+}
+
+void CTPMarketGateWay::OnFrontConnected()
+{
+    m_Logger->Log->info("CTPMarketGateWay::OnFrontConnected 建立网络连接成功");
+    CThostFtdcReqUserLoginField loginReq;
+    memset(&loginReq, 0, sizeof(loginReq));
+    strcpy(loginReq.BrokerID, m_CTPMarketSourceConfig.BrokerID.c_str());
+    strcpy(loginReq.UserID, m_CTPMarketSourceConfig.Account.c_str());
+    strcpy(loginReq.Password, m_CTPMarketSourceConfig.Password.c_str());
+    m_Logger->Log->info("CTPMarketGateWay::OnFrontConnected FrontAddr:{} BrokerID:{} UserID:{}", 
+                        m_CTPMarketSourceConfig.FrontAddr, m_CTPMarketSourceConfig.BrokerID, m_CTPMarketSourceConfig.Account);
+    static int requestID = 0;
+    int rt = m_pMdUserApi->ReqUserLogin(&loginReq, requestID++);
+    if (!rt)
+        m_Logger->Log->info("CTPMarketGateWay::ReqUserLogin 发送登录请求成功");
+    else
+        m_Logger->Log->warn("CTPMarketGateWay::ReqUserLogin 发送登录请求失败");
+}
+
+void CTPMarketGateWay::OnFrontDisconnected(int nReason)
+{
+    char buffer[32] = {0};
+    sprintf(buffer, "0X%X", nReason);
+    m_Logger->Log->warn("CTPMarketGateWay::OnFrontDisconnected 网络连接断开, 错误码:{}", buffer);
+}
+
+void CTPMarketGateWay::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+{
+    bool bResult = pRspInfo && (pRspInfo->ErrorID != 0);
+    if (bIsLast && !bResult)
+    {
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogin 账户登录成功");
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogin 交易日:{}", pRspUserLogin->TradingDay);
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogin 登录时间:{}", pRspUserLogin->LoginTime);
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogin 经纪商:{}", pRspUserLogin->BrokerID);
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogin 账户名:{}", pRspUserLogin->UserID);
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogin SystemName:{}", pRspUserLogin->SystemName);
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogin ApiVersion:{}", m_pMdUserApi->GetApiVersion());
+
+        // 读取合约配置
+        int instrumentNum = m_TickerExchangeMap.size();
+        char *instruments[instrumentNum];
+        int i = 0;
+        for (auto it = m_TickerExchangeMap.begin(); it != m_TickerExchangeMap.end(); ++it)
+        {
+            instruments[i] = new char[32];
+            memset(instruments[i], 0, 32);
+            strncpy(instruments[i], it->first.c_str(), 32);
+            i++;
+        }
+        // 开始订阅行情
+        int rt = m_pMdUserApi->SubscribeMarketData(instruments, instrumentNum);
+        if (!rt)
+            m_Logger->Log->info("CTPMarketGateWay::SubscribeMarketData 发送订阅行情请求成功");
+        else
+            m_Logger->Log->warn("CTPMarketGateWay::SubscribeMarketData 发送订阅行情请求失败");
+
+        for (size_t i = 0; i < instrumentNum; i++)
+        {
+            delete[] instruments[i];
+            instruments[i] = NULL;
+        }
+    }
+    else
+    {
+        m_Logger->Log->warn("CTPMarketGateWay::OnRspUserLogin 返回错误，ErrorID={}, {}", pRspInfo->ErrorID, pRspInfo->ErrorMsg);
+    }
+}
+
+void CTPMarketGateWay::OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+{
+    bool bResult = pRspInfo && (pRspInfo->ErrorID != 0);
+    if (!bResult)
+    {
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogout 账户登出成功");
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogout 经纪商:{}", pUserLogout->BrokerID);
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUserLogout 账户:{}", pUserLogout->UserID);
+    }
+    else
+    {
+        m_Logger->Log->warn("CTPMarketGateWay::OnRspUserLogout 返回错误，ErrorID={}, {}", pRspInfo->ErrorID, pRspInfo->ErrorMsg);
+    }  
+}
+
+void CTPMarketGateWay::OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+{
+    bool bResult = pRspInfo && (pRspInfo->ErrorID != 0);
+    if (bResult)
+    {
+        m_Logger->Log->warn("CTPMarketGateWay::OnRspError 返回错误，ErrorID={}, {}", pRspInfo->ErrorID, pRspInfo->ErrorMsg);
+    }
+}
+
+void CTPMarketGateWay::OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+{
+    bool bResult = pRspInfo && (pRspInfo->ErrorID != 0);
+    if (!bResult && pSpecificInstrument != NULL)
+    {
+        m_Logger->Log->info("CTPMarketGateWay::OnRspSubMarketData 订阅行情成功 合约代码:{}", pSpecificInstrument->InstrumentID);
+    }
+    else
+    {
+        m_Logger->Log->warn("CTPMarketGateWay::OnRspSubMarketData 返回错误，ErrorID={}, {}", pRspInfo->ErrorID, pRspInfo->ErrorMsg);
+    }
+}
+
+void CTPMarketGateWay::OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+{
+    bool bResult = pRspInfo && (pRspInfo->ErrorID != 0);
+    m_Logger->Log->debug("CTPMarketGateWay::OnRspUnSubMarketData ErrorID={}, ErrorMsg={} nRequestID={} bIsLast={}", pRspInfo->ErrorID, pRspInfo->ErrorMsg, nRequestID, bIsLast);
+    if (!bResult)
+    {
+        m_Logger->Log->info("CTPMarketGateWay::OnRspUnSubMarketData 取消订阅行情成功 合约代码:{}", pSpecificInstrument->InstrumentID);
+    }
+    else
+    {
+        m_Logger->Log->warn("CTPMarketGateWay::OnRspUnSubMarketData 返回错误，ErrorID={}, {}", pRspInfo->ErrorID, pRspInfo->ErrorMsg);
+    }
+}
+
+void CTPMarketGateWay::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData)
+{
+    // print Market Data for Debug
+    // PrintDepthMarketData(pDepthMarketData);
+    // 处理订阅合约行情数据
+    std::string recvTime = Utils::getCurrentTimeUs();
+    auto it = m_TickerExchangeMap.find(pDepthMarketData->InstrumentID);
+    if (it != m_TickerExchangeMap.end())
+    {
+        memset(&m_MarketData, 0, sizeof(m_MarketData));
+        memcpy(m_MarketData.RevDataLocalTime, recvTime.c_str(), sizeof(m_MarketData.RevDataLocalTime));
+        strncpy(m_MarketData.ExchangeID, it->second.c_str(), sizeof(m_MarketData.ExchangeID));
+        ParseMarketData(*pDepthMarketData, m_MarketData);
+        std::string datetime= Utils::getCurrentDay();
+        datetime = datetime + " " + m_MarketData.UpdateTime;
+        memcpy(m_MarketData.UpdateTime, datetime.c_str(), sizeof(m_MarketData.UpdateTime));
+        // 写入行情数据到行情队列
+        Message::PackMessage message;
+        message.MessageType = Message::EMessageType::EFutureMarketData;
+        memcpy(&message.FutureMarketData, &m_MarketData, sizeof(message.FutureMarketData));
+        bool ret = m_MarketMessageQueue.push(message);
+        m_Logger->Log->debug("CTPMarketGateWay::OnRtnDepthMarketData Tick:{} Ticker:{} ret={}", 
+                            message.FutureMarketData.Tick, message.FutureMarketData.Ticker, ret);
+    }
+}
+
+void CTPMarketGateWay::ParseMarketData(const CThostFtdcDepthMarketDataField& depthMarketData, MarketData::TFutureMarketData &tickData)
+{
+    // numeric_limits<double>::max()
+    strncpy(tickData.Ticker, depthMarketData.InstrumentID, sizeof(tickData.Ticker));
+    strncpy(tickData.UpdateTime, depthMarketData.UpdateTime, sizeof(tickData.UpdateTime));
+    tickData.MillSec = depthMarketData.UpdateMillisec;
+    tickData.LastPrice = depthMarketData.LastPrice;
+    tickData.Volume = depthMarketData.Volume;
+    tickData.Turnover = depthMarketData.Turnover;
+    tickData.PreSettlementPrice = depthMarketData.PreSettlementPrice;
+    tickData.PreClosePrice = depthMarketData.PreClosePrice;
+    tickData.OpenInterest = depthMarketData.OpenInterest;
+    tickData.OpenPrice = depthMarketData.OpenPrice;
+    tickData.HighestPrice = depthMarketData.HighestPrice;
+    tickData.LowestPrice = depthMarketData.LowestPrice;
+    tickData.UpperLimitPrice = depthMarketData.UpperLimitPrice;
+    tickData.LowerLimitPrice = depthMarketData.LowerLimitPrice;
+
+    tickData.BidPrice1 = depthMarketData.BidPrice1;
+    tickData.BidVolume1 = depthMarketData.BidVolume1;
+    tickData.AskPrice1 = depthMarketData.AskPrice1;
+    tickData.AskVolume1 = depthMarketData.AskVolume1;
+
+    tickData.BidPrice2 = depthMarketData.BidPrice2;
+    tickData.BidVolume2 = depthMarketData.BidVolume2;
+    tickData.AskPrice2 = depthMarketData.AskPrice2;
+    tickData.AskVolume2 = depthMarketData.AskVolume2;
+
+    tickData.BidPrice3 = depthMarketData.BidPrice3;
+    tickData.BidVolume3 = depthMarketData.BidVolume3;
+    tickData.AskPrice3 = depthMarketData.AskPrice3;
+    tickData.AskVolume3 = depthMarketData.AskVolume3;
+
+    tickData.BidPrice4 = depthMarketData.BidPrice4;
+    tickData.BidVolume4 = depthMarketData.BidVolume4;
+    tickData.AskPrice4 = depthMarketData.AskPrice4;
+    tickData.AskVolume4 = depthMarketData.AskVolume4;
+
+    tickData.BidPrice5 = depthMarketData.BidPrice5;
+    tickData.BidVolume5 = depthMarketData.BidVolume5;
+    tickData.AskPrice5 = depthMarketData.AskPrice5;
+    tickData.AskVolume5 = depthMarketData.AskVolume5;
+}
+
+void CTPMarketGateWay::PrintDepthMarketData(const CThostFtdcDepthMarketDataField *pDepthMarketData)
+{
+    m_Logger->Log->debug("====================================={}===============================", pDepthMarketData->InstrumentID);
+    m_Logger->Log->debug("TradingDay {}", pDepthMarketData->TradingDay);
+    m_Logger->Log->debug("ExchangeID {}", pDepthMarketData->ExchangeID);
+    m_Logger->Log->debug("Ticker {}", pDepthMarketData->InstrumentID);
+    m_Logger->Log->debug("ExchangeInstID {}", pDepthMarketData->ExchangeInstID);
+    m_Logger->Log->debug("LastPrice {}", pDepthMarketData->LastPrice);
+    m_Logger->Log->debug("Volume {}", pDepthMarketData->Volume);
+    m_Logger->Log->debug("PreSettlementPrice {}", pDepthMarketData->PreSettlementPrice);
+    m_Logger->Log->debug("PreClosePrice {}", pDepthMarketData->PreClosePrice);
+    m_Logger->Log->debug("PreOpenInterest {}", pDepthMarketData->PreOpenInterest);
+    m_Logger->Log->debug("OpenPrice {}", pDepthMarketData->OpenPrice);
+    m_Logger->Log->debug("HighestPrice {}", pDepthMarketData->HighestPrice);
+    m_Logger->Log->debug("LowestPrice {}", pDepthMarketData->LowestPrice);
+    m_Logger->Log->debug("Turnover {}", pDepthMarketData->Turnover);
+    m_Logger->Log->debug("OpenInterest {}", pDepthMarketData->OpenInterest);
+    m_Logger->Log->debug("ClosePrice {}", pDepthMarketData->ClosePrice);
+    m_Logger->Log->debug("SettlementPrice {}", pDepthMarketData->SettlementPrice);
+    m_Logger->Log->debug("UpperLimitPrice {}", pDepthMarketData->UpperLimitPrice);
+    m_Logger->Log->debug("PreDelta {}", pDepthMarketData->PreDelta);
+    m_Logger->Log->debug("CurrDelta {}", pDepthMarketData->CurrDelta);
+    m_Logger->Log->debug("UpdateTime {}", pDepthMarketData->UpdateTime);
+    m_Logger->Log->debug("UpdateMillisec {}", pDepthMarketData->UpdateMillisec);
+    m_Logger->Log->debug("BidPrice1 {}", pDepthMarketData->BidPrice1);
+    m_Logger->Log->debug("BidVolume1 {}", pDepthMarketData->BidVolume1);
+    m_Logger->Log->debug("AskPrice1 {}", pDepthMarketData->AskPrice1);
+    m_Logger->Log->debug("AskVolume1 {}", pDepthMarketData->AskVolume1);
+    m_Logger->Log->debug("BidPrice2 {}", pDepthMarketData->BidPrice2);
+    m_Logger->Log->debug("BidVolume2 {}", pDepthMarketData->BidVolume2);
+    m_Logger->Log->debug("AskPrice2 {}", pDepthMarketData->AskPrice2);
+    m_Logger->Log->debug("AskVolume2 {}", pDepthMarketData->AskVolume2);
+    m_Logger->Log->debug("BidPrice3 {}", pDepthMarketData->BidPrice3);
+    m_Logger->Log->debug("BidVolume3 {}", pDepthMarketData->BidVolume3);
+    m_Logger->Log->debug("AskPrice3 {}", pDepthMarketData->AskPrice3);
+    m_Logger->Log->debug("AskVolume3 {}", pDepthMarketData->AskVolume3);
+    m_Logger->Log->debug("BidPrice4 {}", pDepthMarketData->BidPrice4);
+    m_Logger->Log->debug("BidVolume4 {}", pDepthMarketData->BidVolume4);
+    m_Logger->Log->debug("AskPrice4 {}", pDepthMarketData->AskPrice4);
+    m_Logger->Log->debug("AskVolume4 {}", pDepthMarketData->AskVolume4);
+    m_Logger->Log->debug("BidPrice5 {}", pDepthMarketData->BidPrice5);
+    m_Logger->Log->debug("BidVolume5 {}", pDepthMarketData->BidVolume5);
+    m_Logger->Log->debug("AskPrice5 {}", pDepthMarketData->AskPrice5);
+    m_Logger->Log->debug("AskVolume5 {}", pDepthMarketData->AskVolume5);
+    m_Logger->Log->debug("AveragePrice {}", pDepthMarketData->AveragePrice);
+    m_Logger->Log->debug("ActionDay {}", pDepthMarketData->ActionDay);
+}
---
+++ MarketAPI/CTP/CTPMarketGateWay.h
@@ -0,0 +1,62 @@
+#ifndef CTPMARKETGATEWAY_H
+#define CTPMARKETGATEWAY_H
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <string.h>
+#include <stdio.h>
+#include "ThostFtdcMdApi.h"
+#include "YMLConfig.hpp"
+#include "Logger.h"
+#include "MarketGateWay.hpp"
+#include "PackMessage.hpp"
+
+class CTPMarketGateWay : public MarketGateWay, public CThostFtdcMdSpi
+{
+public:
+    explicit CTPMarketGateWay();
+public:
+    virtual bool LoadAPIConfig();
+    virtual void Run();
+    virtual void GetCommitID(std::string& CommitID, std::string& UtilsCommitID);
+    virtual void GetAPIVersion(std::string& APIVersion);
+public:
+    /*********************************************************************************
+     *  API callback implementation
+     * ******************************************************************************/
+    ///当客户端与交易后台建立起通信连接时（还未登录前）被调用。
+    void OnFrontConnected();
+
+    ///当客户端与交易后台通信连接断开时被调用。API会自动重新连接，客户端可不做处理。
+    ///@param nReason 错误原因
+    void OnFrontDisconnected(int nReason);
+
+    ///登录请求响应
+    void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+    ///登出请求响应
+    void OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+    ///错误应答
+    void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+    ///订阅行情应答
+    void OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+    ///取消订阅行情应答
+    void OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+    ///深度行情通知
+    void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData);
+private:
+    // parse Market Data
+    void ParseMarketData(const CThostFtdcDepthMarketDataField& depthMarketData, MarketData::TFutureMarketData& tickData);
+    void PrintDepthMarketData(const CThostFtdcDepthMarketDataField* pDepthMarketData);
+private:
+    MarketData::TFutureMarketData m_MarketData;
+    CThostFtdcMdApi *m_pMdUserApi;
+    Utils::CTPMarketSourceConfig m_CTPMarketSourceConfig;
+};
+
+#endif // CTPMARKETGATEWAY_H
